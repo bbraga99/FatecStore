0c1545238a06a1fffd9dde490a441e6fb9fcc95b
braga <brunuu.henrique@hotmail.com>
2025-05-07 06:07:04 -0300

    Adicionando couponTotalDTO

---
+++ LojaFatecDiscountAPI/DTOs/CouponTotalDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Fatec.Store.Discount.Api.DTOs
+{
+    public class CouponTotalDTO
+    {
+        public double Total { get; set; }
+        public double Discount { get; set; }
+        public double FinalTotal { get; set; }
+    }
+}
