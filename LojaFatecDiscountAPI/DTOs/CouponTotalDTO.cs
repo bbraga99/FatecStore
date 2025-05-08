5cbbcfef627549e1c56f16dcb3b80f2c0629277e
braga <brunuu.henrique@hotmail.com>
2025-05-08 19:32:35 -0300

    removendo cartTotalDTO e service

--- LojaFatecDiscountAPI/DTOs/CouponTotalDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Fatec.Store.Discount.Api.DTOs
-{
-    public class CouponTotalDTO
-    {
-        public double Total { get; set; }
-        public double Discount { get; set; }
-        public double FinalTotal { get; set; }
-    }
-}
